ed4890094b62317782324b43252b6e8ad7e5aae0
XxGhoulPr0xX <javieralvaradomarquez1102@gmail.com>
2026-01-20 17:03:11 -0600

    Actualizando documentación y código, ignorando temporales

--- Programacion/src/Service/Servicios.py
+++ Programacion/src/Service/Servicios.py
@@ -3,5 +3,5 @@ from src.Service.ManejadorHTTP import Manejador
 from src.Service.HistorialResultados import estadisticasIdentificaciones
 
 # Inicialización de componentes globales
-alpha = Manejador(modelo="C:\\Users\\XxGho\\OneDrive\\Documentos\\Escuela\\Proceso Dual\\Proyecto\\3° Proyecto\\Programacion\\static\\Modelos\\Identificacion de images\\model_retrained_REALDATA_v2.h5")
+alpha = Manejador(modelo="C:\\Users\\XxGho\\OneDrive\\Documentos\\Escuela\\Proceso Dual\\Proyecto\\3° Proyecto\\Programacion\\static\\Modelos\\Identificacion de images\\model_retrained_REALDATA.h5")
 charlie = estadisticasIdentificaciones()
